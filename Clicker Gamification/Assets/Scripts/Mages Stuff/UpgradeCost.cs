05a6352857051e1f6217424171ffac4600d46bbf
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-05-18 13:56:37 +1000

    Skeleton and Damage

---
+++ Clicker Gamification/Assets/Scripts/Mages Stuff/UpgradeCost.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeCost : MonoBehaviour {
+
+    Text upgradeFire;
+    int cost = 50;
+
+	// Use this for initialization
+	void Start ()
+    {
+        upgradeFire = GetComponent<Text>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        upgradeFire.text = "Cost to upgrade: " + cost;
+	}
+    public bool Upgrade()
+    {
+        if((GameManager.instance.gold - cost) >= 0)
+        {
+            GameManager.instance.SpendGold(cost);
+            cost += 50;//Increase cost
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- Clicker Gamification/Assets/Scripts/TapFoe.cs
+++ Clicker Gamification/Assets/Scripts/TapFoe.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TapFoe : MonoBehaviour {
-
+public class TapFoe : MonoBehaviour
+{
+    int health = 50;
+    public Slider enemyHealth;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        enemyHealth.value = health;
 	}
+    public void Tap()
+    {
+        health--;
+    }
 }
