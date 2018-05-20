ce1b5c5bc8f3fa92fb83c2456c6a946bc5d56d09
winter <jaradm552@gmail.com>
2018-05-20 12:23:51 +1000

    scroll screen changes, reading scroll added but not finished

---
+++ Clicker Gamification/Assets/Scripts/OnButonClick_Scrolls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnButonClick_Scrolls : MonoBehaviour {
+
+    //Buttons
+    public Button readingScroll, mathsScroll, back, newScroll;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //go to reading scroll
+    public void OnClick_Reading() {
+    
+        SceneManager.LoadScene("Reading Scroll");
+        
+    }
+
+    //go to maths scroll
+    public void OnClick_Maths() {
+    
+        SceneManager.LoadScene("Calculus Scroll");
+        
+    }
+
+    //go back to previous page
+    public void OnClick_Back() { 
+    
+    }
+
+    //go to scroll creation page
+    public void OnClick_NewScroll() { 
+        
+    }
+}
---
+++ Clicker Gamification/Assets/Scripts/OnClick_Scrolls_Continue.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnClick_Scrolls_Continue : MonoBehaviour {
+
+    //Canvases
+    public Canvas Reading, Q1, Q2, Q3, Q4;
+    public List<Canvas> CanvasList;
+    private int listno = 0;
+	// Use this for initialization
+	void Start () {
+		CanvasList = new List<Canvas>{Reading, Q1, Q2, Q3, Q4};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print(CanvasList[listno]);
+        ChangeVisableCanvas();
+	}
+
+    //check if listno > 4 and jump back to scroll selection if so otherwise increment listno.
+    public void OnClick_Continue() { 
+        if (listno > 4) { 
+        
+            SceneManager.LoadScene("Scroll Screen");
+        
+        } else {
+    
+            listno += 1;
+    
+        }
+         
+    }
+
+    //check if listno < 0 and jump back to scroll selection if so otherwise decrement listno.
+    public void OnClick_Back() { 
+        if (listno < 0) { 
+        
+            SceneManager.LoadScene("Scroll Screen");
+        
+        } else {
+    
+            listno -= 1;
+    
+        }
+         
+    }
+
+    //changes which of the canvases is visable
+    private void ChangeVisableCanvas() { 
+
+        //if Reading is the active canvas
+        if (listno == 0) {
+    
+            Reading.GetComponent<Canvas>().enabled = true;
+            Q1.GetComponent<Canvas>().enabled = false;
+            Q2.GetComponent<Canvas>().enabled = false;
+            Q3.GetComponent<Canvas>().enabled = false;
+            Q4.GetComponent<Canvas>().enabled = false;
+    
+        }
+
+        //if Q1 is the active canvas
+        if (listno == 1) {
+    
+            Reading.GetComponent<Canvas>().enabled = false;
+            Q1.GetComponent<Canvas>().enabled = true;
+            Q2.GetComponent<Canvas>().enabled = false;
+            Q3.GetComponent<Canvas>().enabled = false;
+            Q4.GetComponent<Canvas>().enabled = false;
+    
+        }
+
+        //if Q2 is the active canvas
+        if (listno == 2) {
+    
+            Reading.GetComponent<Canvas>().enabled = false;
+            Q1.GetComponent<Canvas>().enabled = false;
+            Q2.GetComponent<Canvas>().enabled = true;
+            Q3.GetComponent<Canvas>().enabled = false;
+            Q4.GetComponent<Canvas>().enabled = false;
+    
+        }
+
+        //if Q3 is the active canvas
+        if (listno == 3) {
+    
+            Reading.GetComponent<Canvas>().enabled = false;
+            Q1.GetComponent<Canvas>().enabled = false;
+            Q2.GetComponent<Canvas>().enabled = false;
+            Q3.GetComponent<Canvas>().enabled = true;
+            Q4.GetComponent<Canvas>().enabled = false;
+    
+        }
+
+        //if Q4 is the active canvas
+        if (listno >= 4) {
+    
+            Reading.GetComponent<Canvas>().enabled = false;
+            Q1.GetComponent<Canvas>().enabled = false;
+            Q2.GetComponent<Canvas>().enabled = false;
+            Q3.GetComponent<Canvas>().enabled = false;
+            Q4.GetComponent<Canvas>().enabled = true;
+    
+        }
+    } 
+}
