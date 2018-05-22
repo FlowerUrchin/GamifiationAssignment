2ebb2b492e525a9df15e730778a4c6d536b3f8f3
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-05-22 14:58:28 +1000

    Combat Screen

---
+++ Clicker Gamification/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void hit()
+    {
+        GameManager.instance.foes[GameManager.instance.foe-1].GetComponent<TapFoe>().Tap();
+    }
+}
