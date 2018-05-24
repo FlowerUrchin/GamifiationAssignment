e78c01e342761ba7879fc2a8c6e51f8a73ed7f39
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-05-24 13:48:19 +1000

    Fire Bear and Flashing Number

---
+++ Clicker Gamification/Assets/Scripts/Mages Stuff/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+
+    public Text damage;
+    int dam, curText = 0;
+    float time = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Time.time >= time)
+        {
+            damage.text = "";
+        }		
+	}
+    public void hit()
+    {
+        dam = GameManager.instance.foes[GameManager.instance.FoeInt()].GetComponent<TapFoe>().Tap();
+        damage.text = "" + dam;
+        time = Time.time + 0.07f;
+
+    }
+}
