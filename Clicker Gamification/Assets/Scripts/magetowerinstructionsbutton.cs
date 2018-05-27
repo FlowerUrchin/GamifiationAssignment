1b2546706d15aaccba4d336f12f795c25cbf6262
winter <jaradm552@gmail.com>
2018-05-27 15:35:58 +1000

    extra bits

---
+++ Clicker Gamification/Assets/Scripts/magetowerinstructionsbutton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magetowerinstructionsbutton : MonoBehaviour {
+    public Canvas instructions;
+	// Use this for initialization
+	void Start () {
+        print(instructions.enabled);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnOffInstructions() { 
+    
+       if (instructions.enabled == false) { 
+    
+            instructions.enabled = true;
+            print("reached 1");
+            print(instructions.enabled);
+
+       } 
+       
+       else if (instructions.enabled == true) { 
+    
+            instructions.enabled = false;
+            print("reached 2");
+            print(instructions.enabled);
+        }
+
+    }
+}
